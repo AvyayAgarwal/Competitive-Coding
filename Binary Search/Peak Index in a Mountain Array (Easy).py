efed96c4790b0d2cc9c851f9257a520bb72fc2ef
Avyay Agarwal <30388692+AvyayAgarwal@users.noreply.github.com>
2020-06-16 00:25:23 +0530

    Easy x1 updated, Medium x1 updated

--- Binary Search/Peak Index in a Mountain Array (Easy).py
+++ Binary Search/Peak Index in a Mountain Array (Easy).py
@@ -5,10 +5,7 @@ class Solution:
     def recurse(self, nums: List[int], low: int, high: int):
         mid = (low + high) // 2
         if(low == high):
-            if(nums[low] >= nums[high]):
-                return low
-            else:
-                return high
+            return low
         elif(nums[mid] >= nums[mid+1]):
             return self.recurse(nums, low, mid)
         else:
