fc2bc538b22f6512fa0b3a37f2b70e938ea29019
Avyay Agarwal <30388692+AvyayAgarwal@users.noreply.github.com>
2020-06-15 19:46:32 +0530

    Medium x1

---
+++ Binary Search/ Find Peak Element (Medium).py
@@ -0,0 +1,19 @@
+# 162. Find Peak Element - Medium
+# Tags - Array, Binary Search
+
+class Solution:
+    def recurse(self, nums: List[int], low: int, high: int):
+        mid = (low + high) // 2
+        if(low == high):
+            if(nums[low] >= nums[high]):
+                return low
+            else:
+                return high
+        elif(nums[mid] >= nums[mid+1]):
+            return self.recurse(nums, low, mid)
+        else:
+            return self.recurse(nums, mid + 1, high)
+       
+        
+    def findPeakElement(self, nums: List[int]) -> int:
+        return self.recurse(nums, 0, len(nums)-1)
