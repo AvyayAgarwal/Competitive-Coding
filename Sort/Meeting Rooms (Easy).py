6f6f79abf438e63201ab6c012e45c9592d3e5245
Avyay Agarwal <30388692+AvyayAgarwal@users.noreply.github.com>
2020-09-01 20:27:21 +0530

    Easy x1, Medium x1

---
+++ Sort/Meeting Rooms (Easy).py
@@ -0,0 +1,11 @@
+# 252. Meeting Rooms - Easy
+# Tags - Sort, Greedy (Not listed)
+
+class Solution:
+    def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
+        intervals.sort(key=lambda x: x[0])
+    
+        for i in range(len(intervals)-1):
+            if intervals[i][1] > intervals[i+1][0]:
+                return False
+        return True
